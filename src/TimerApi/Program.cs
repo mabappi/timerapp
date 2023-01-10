ff42ba6ae7667d29b8dadca76133f0f1603a50f6
Bappi Ahmed <bappi.ahmed@collenda.com>
2023-01-10 11:47:42 +0100

    Added Swagger

--- src/TimerApi/Program.cs
+++ src/TimerApi/Program.cs
@@ -1,17 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services
+    .AddEndpointsApiExplorer()
+    .AddSwaggerGen(c => { c.SwaggerDoc("v1", new() { Title = "Timer Api", Version = "v1" }); });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
-//app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+    app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
